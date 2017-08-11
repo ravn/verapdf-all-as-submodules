6c8fbe58dcef7d0c9b46fc84eb43c16e50554a05
tra <tra@kb.dk>
2017-08-11 17:06:12 +0200

    Got the dependencies to work with the help of ABR.

--- repos.pl
+++ repos.pl
@@ -23,3 +23,8 @@ while(<DATA>) {
 __DATA__
 # format:  directory_to_clone_to  url_to_clone_from  git_commit_id_to_checkout
 # 
+modules/veraPDF-library-integration https://github.com/veraPDF/veraPDF-library integration
+modules/veraPDF-library-integration/parent/veraPDF-validation https://github.com/veraPDF/veraPDF-validation integration
+modules/veraPDF-library-integration/parent/veraPDF-model https://github.com/veraPDF/veraPDF-model integration
+modules/veraPDF-library-integration/parent/veraPDF-parser https://github.com/veraPDF/veraPDF-parser integration
+
