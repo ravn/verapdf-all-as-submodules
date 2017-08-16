cf71b4efba849678834f113ebefe26ca97f08bac
tra <tra@kb.dk>
2017-08-16 12:55:55 +0200

    Made TestOnSynthetcFiles work.  iText nags and is so annoying that this needs to go.

--- it/verapdf-kb-qa/src/test/java/dk/kb/verapdf/qa/TestOnSyntheticFiles.java
+++ it/verapdf-kb-qa/src/test/java/dk/kb/verapdf/qa/TestOnSyntheticFiles.java
@@ -3,11 +3,14 @@ package dk.kb.verapdf.qa;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import dk.statsbiblioteket.digital_pligtaflevering_aviser.verapdf.VeraPDFValidator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
@@ -16,50 +19,55 @@ import java.io.OutputStream;
  */
 public class TestOnSyntheticFiles {
 
-    public byte[] createSimplePdf(String paragraph) throws Exception {
+    public byte[] createSimplePdf(String paragraph) {
+        try {
+            // http://developers.itextpdf.com/examples/itext-action-second-edition/chapter-1
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        // http://developers.itextpdf.com/examples/itext-action-second-edition/chapter-1
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Document document = new Document();
+            //PdfAWriter.getInstance(document, baos, PdfAConformanceLevel.PDF_A_1A);
+            PdfWriter.getInstance(document, baos);
+            document.open();
+            document.add(new Paragraph(paragraph));
+            document.close();
 
-        Document document = new Document();
-        //PdfAWriter.getInstance(document, baos, PdfAConformanceLevel.PDF_A_1A);
-        PdfWriter.getInstance(document, baos);
-        document.open();
-        document.add(new Paragraph(paragraph));
-        document.close();
-
-        try(OutputStream os = new FileOutputStream("/tmp/x.pdf")) {
-            baos.writeTo(os);
+            try (OutputStream os = new FileOutputStream("/tmp/x.pdf")) {
+                baos.writeTo(os);
+            }
+            return baos.toByteArray();
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return baos.toByteArray();
     }
+
     @Test
     public void checkForLeakingFileDescriptorsOnSyntheticFiles() throws Exception {
         Assert.assertTrue(createSimplePdf("Hello world").length > 0);
-//        long processed = 0;
-//
-//        long processLimit = 100_000;
-//
-//        while (true) {
-//            for (Path p : pdfPaths) {
-//                if (processed++ > processLimit) {
-//                    Assert.assertTrue(true);
-//                    return; // ok.
-//                }
-//                if (processed % 100 == 0) {
-//                    System.out.println("*** " + processed);
-//                }
-//
-//                try {
-//                    final InputStream inputStream = Files.newInputStream(p);
-//                    byte[] b = validator.apply(inputStream);
-//                    inputStream.close();
-//                    System.out.println(p + " " + b.length);
-//                } catch (Exception e) {
-//                    System.err.println(p + " failed: " + e.getMessage());
-//                    e.printStackTrace(System.err);
-//                }
-//            }
-//        }
+
+        long processed = 0;
+
+        long processLimit = 100_000;
+
+        VeraPDFValidator validator = new VeraPDFValidator("1b", true);
+
+        while (true) {
+            if (processed++ > processLimit) {
+                Assert.assertTrue(true);
+                return; // ok.
+            }
+
+            byte[] simplePdf = createSimplePdf("Hello #" + processed);
+            try {
+                final InputStream inputStream = new ByteArrayInputStream(simplePdf);
+                byte[] b = validator.apply(inputStream);
+                inputStream.close();
+            } catch (Exception e) {
+                System.err.println(processed + " failed: " + e.getMessage());
+                e.printStackTrace(System.err);
+            }
+        }
     }
 }
+
